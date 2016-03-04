065b02c590fc83237c58ef21745194b6fd702c42
Jasmine Woo <jwoo@cs.ualberta.ca>
2016-03-04 12:57:47 -0700

    testing oracle connection. I'll clean this up in the next commit.

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 
 var express = require('express'),
     app = express(),
-    http = require('http');
+    http = require('http'),
+    oracledb = require('oracledb');
 
 //Set port number. This will be changed to the port assigned to us
 app.set('port', 8080);
@@ -17,6 +18,46 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(request, response) {
   response.render('view/index');
 });
+
+oracledb.getConnection(
+  {
+    user          : "jwoo",
+    password      : "", //Put password here (I'll message you my password :P)
+    connectString : "CRS" //16040
+  },
+  function(err, connection)
+  {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    connection.execute(
+      "SELECT * " +
+      "FROM COURSE ",
+      function(err, result)
+      {
+        if (err) {
+          console.error(err.message);
+          doRelease(connection);
+          return;
+        }
+        console.log(result.metaData);
+        console.log(result.rows);
+        doRelease(connection);
+      });
+  });
+
+function doRelease(connection)
+{
+  connection.release(
+    function(err) {
+      if (err) {
+        console.error(err.message);
+      }
+    });
+}
+
+
 
 //Server
 http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
