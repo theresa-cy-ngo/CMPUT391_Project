2a6c828e87cfc38fc5cd42edb5b4f5dcee302f09
Jasmine Woo <jazzmosis12@gmail.com>
2016-03-05 20:37:56 -0700

    Connect to Oracle

---
+++ dbConfig.js
@@ -0,0 +1,7 @@
+module.exports = {
+    user: "jwoo",
+    password: "", //Put password here (I'll message you my password :P)
+    connectString: ""
+    //if WFH then localhost:1525/CRS
+    //if lab machine then CRS
+};
--- server.js
+++ server.js
@@ -2,16 +2,16 @@
 
 var express = require('express'),
     app = express(),
-    http = require('http');
+    http = require('http'),
+    oracledb = require('oracledb'),
+    dbConfig = require('./dbconfig.js');
 
-//Set port number. This will be changed to the port assigned to us
+//Set port number.
 app.set('port', 8080);
 app.set('ip', "127.0.0.1");
 
-
 //Add public folder for static files
 app.use(express.static(__dirname + '/public'));
-
 
 //Set first page
 app.get('/', function(request, response) {
@@ -22,3 +22,76 @@ app.get('/', function(request, response) {
 http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
     console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
 });
+
+
+//Get Oracle connection (test function)
+oracledb.getConnection(dbConfig, function(err, connection) {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    connection.execute(
+      "SELECT * " +
+      "FROM COURSE ",
+      function(err, result)
+      {
+        if (err) {
+          console.error(err.message);
+          doRelease(connection);
+          return;
+        }
+        console.log(result.metaData);
+        console.log(result.rows);
+        doRelease(connection);
+      });
+  });
+
+//Disconnect from Oracle
+function doRelease(connection)
+{
+  connection.release(
+    function(err) {
+      if (err) {
+        console.error(err.message);
+      }
+    });
+}
+
+
+/**
+* Connection Errors
+*/
+
+// function connectionError(err, res) {
+//     console.error(err.message);
+//     res.status(500);
+//     res.send(
+//         {
+//             error: true,
+//             errorCode: 1,
+//             message: "Unable to connect to database. Please try again"
+//         }
+//     );
+// }
+//
+// function executeError(err, res) {
+//     console.log(err.message);
+//     res.status(500);
+//     res.send(
+//         {
+//             error: true,
+//             errorCode: 3,
+//             message: "There was a problem retrieving data. Please try again",
+//             success: false
+//         }
+//     );
+// }
+//
+// function notAuthorized(res) {
+//     res.status(401);
+//     res.send({
+//         error: true,
+//         errorCode: 99,
+//         message: "You are not authorized to view this data"
+//     });
+// }
