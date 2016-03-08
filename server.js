297ba50da1b6dab19f828b170ad277c2698b069d
Jasmine Woo <jazzmosis12@gmail.com>
2016-03-08 13:24:56 -0700

    Semi-working login. Use "test1" as username and password

--- server.js
+++ server.js
@@ -6,9 +6,15 @@ var express = require('express'),
     oracledb = require('oracledb'),
     dbConfig = require('./dbconfig.js'),
     bodyParser = require('body-parser'),
-    ejs = require('ejs');
+    cookieParser = require("cookie-parser"),
+    ejs = require('ejs'),
+    util = require('util');
+
+
+
 
 app.set('view engine', ejs);
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(function (req, res, next) {
@@ -34,16 +40,20 @@ app.set('port', 8080);
 app.set('ip', "127.0.0.1");
 
 //Add public folder for static files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/src'));
 
 //Set first page
 app.get('/', function(request, response) {
-  response.render(__dirname + '/public/index');
+  response.render(__dirname + '/src/index');
 });
 
 //Server
 http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
     console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
+});
+
+process.on('SIGINT', function() {
+  process.exit();
 });
 
 
@@ -69,9 +79,51 @@ http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
 //       });
 //   });
 
+
+app.post("/login", function (req, res) {
+    oracledb.getConnection(dbConfig, function (err, connection) {
+            if (err) {
+                connectionError(err, res);
+                return;
+            }
+            connection.execute(
+                "SELECT * " +
+                "FROM users " +
+                "WHERE user_name = :user_name AND password = :password",
+                {user_name: req.body.userName, password: req.body.password},
+                function (err, result) {
+                    if (err) {
+                        executeError(err, res);
+                    } else {
+                        console.log(util.inspect(result, {showHidden: false, depth: null}));
+                        if(result.rows.length > 0) {
+                            // // Set cookies
+                            // res.cookie("user_name", req.body.userName);
+                            // res.cookie("login", true);
+                            res.send({success: true});
+                        } else {
+                            res.status(422);
+                            res.send(
+                                {
+                                    error: true,
+                                    errorCode: 2,
+                                    message: "Username or password is incorrect. Please try again.",
+                                    success: false
+                                }
+                            );
+                        }
+                    }
+                    doRelease(connection);
+                }
+            );
+        }
+    );
+});
+
 //Disconnect from Oracle
 function doRelease(connection)
 {
+  console.log("connect release");
   connection.release(
     function(err) {
       if (err) {
@@ -85,36 +137,36 @@ function doRelease(connection)
 * Connection Errors
 */
 
-// function connectionError(err, res) {
-//     console.error(err.message);
-//     res.status(500);
-//     res.send(
-//         {
-//             error: true,
-//             errorCode: 1,
-//             message: "Unable to connect to database. Please try again"
-//         }
-//     );
-// }
-//
-// function executeError(err, res) {
-//     console.log(err.message);
-//     res.status(500);
-//     res.send(
-//         {
-//             error: true,
-//             errorCode: 3,
-//             message: "There was a problem retrieving data. Please try again",
-//             success: false
-//         }
-//     );
-// }
-//
-// function notAuthorized(res) {
-//     res.status(401);
-//     res.send({
-//         error: true,
-//         errorCode: 99,
-//         message: "You are not authorized to view this data"
-//     });
-// }
+function connectionError(err, res) {
+    console.error(err.message);
+    res.status(500);
+    res.send(
+        {
+            error: true,
+            errorCode: 1,
+            message: "Unable to connect to database. Please try again"
+        }
+    );
+}
+
+function executeError(err, res) {
+    console.log(err.message);
+    res.status(500);
+    res.send(
+        {
+            error: true,
+            errorCode: 3,
+            message: "There was a problem retrieving data. Please try again",
+            success: false
+        }
+    );
+}
+
+function notAuthorized(res) {
+    res.status(401);
+    res.send({
+        error: true,
+        errorCode: 99,
+        message: "You are not authorized to view this data"
+    });
+}
---
+++ src/constants/appConstants.js
@@ -0,0 +1,3 @@
+module.exports = {
+  SERVICE_BASE_URL: 'http://localhost:8080/'
+};
